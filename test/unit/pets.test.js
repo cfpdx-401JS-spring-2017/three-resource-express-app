671c05a447097725c9ac4f2ee4480f41ef420caf
Martin <martinconradpelayo@gmail.com>
2017-04-26 16:38:12 -0700

    woked on pet.test.js tests

---
+++ test/unit/pets.test.js
@@ -0,0 +1,36 @@
+const assert  = require('assert');
+const Pet = require('../../lib/models/pets');
+
+const expectedValidation = () => {throw new Error('expected errors but recieved none');};
+
+describe('validates pets', () => {
+  it('validates model', () => {
+    const pet = new Pet({
+      name: 'bob',
+      legs: 4,
+
+    });
+    return pet.validate();
+
+  });
+
+  describe('validation falures', () => {
+    it('name and legs are required', () => {
+      const pet = new Pet();
+      return pet.validate()
+      .then(expectedValidation,
+      err => {
+        const errors = err.errors;
+        assert.ok(errors.legs && errors.legs.kind === 'required');
+        assert.ok(errors.name && errors.name.kind === 'required');
+      });
+
+
+
+    });
+
+  });
+
+});
+
+
