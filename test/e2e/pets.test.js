280b31af61e0ea1d81401233132b3503dcac8bb6
Martin <martinconradpelayo@gmail.com>
2017-04-26 16:17:55 -0700

    first test working

---
+++ test/e2e/pets.test.js
@@ -0,0 +1,16 @@
+const db = require('./_db');
+const request = require('./_request');
+const assert = require('chai').assert;
+
+describe('pets api', () => {
+  before(db.drop);
+  it('initial /GET returns empty list', () => {
+    return request.get('/api/pets')
+    .then(req => {
+      const pets = req.body;
+      assert.deepEqual(pets, []);
+    });
+  });
+
+
+});
