6a33d72b42915f7623cd47fc9412305025123f6c
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-26 16:47:49 -0700

    Cookie get, post and getById tests passing

--- test/e2e/cookies.test.js
+++ test/e2e/cookies.test.js
@@ -13,4 +13,44 @@ describe('Cookies API', () => {
       assert.deepEqual(cookies, []);
     });
   });
+
+  let courierCookie = {
+    name: 'Courier Cookie',
+    flavor: 'chocolate chip',
+    amount: 12
+  };
+
+  let starbucksCookie = {
+    name: 'Starbucks Cookie',
+    flavor: 'ginger',
+    amount: 45
+  };
+
+  let codeFellowCookie = {
+    name: 'Code Fellow Cookie',
+    flavor: 'oatmeal',
+    amount: 78
+  };
+
+  function saveCookie(cookie) {
+    return request.post('/api/cookies')
+    .send(cookie)
+    .then(res => res.body);
+  }
+
+  it('saves a cookie', () => {
+    return saveCookie(courierCookie)
+    .then(saved => {
+      assert.ok(saved._id, 'saved has id');
+      courierCookie = saved;
+    })
+    .then(() => {
+      return request.get(`/api/cookies/${courierCookie._id}`);
+    })
+    .then(res => res.body)
+    .then(cookie => {
+      assert.deepEqual(cookie, courierCookie);
+    });
+  });
+
 });
