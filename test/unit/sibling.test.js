3c82ebda0791e788fafef0311ab81628bf6cdfc4
Ivy Patton <lilaivy@gmail.com>
2017-04-26 22:52:30 -0700

    sibling unit test passing requires name

---
+++ test/unit/sibling.test.js
@@ -0,0 +1,28 @@
+const assert = require('chai').assert;
+const Sibling = require('../../lib/models/sibling');
+
+const expectedValidation = () => { throw new Error('expected validation errors'); };
+
+describe('siblings model', () => { //Q:why no assert in this describe?
+    const sibling = new Sibling({
+        name: 'fakeSibling',
+        likes: ['fake stuff', 'lame stuff']
+    });
+    return sibling.validate(); //validate is a method included in mongoose
+});
+
+describe('validation failures', () => {
+
+    it('requires name', () => {
+        const sibling = new Sibling();
+        return sibling.validate()
+            .then(expectedValidation,
+            err => {
+                const errors = err.errors; //Q: not following this
+                assert.ok(errors.name && errors.name.kind === 'required');
+            });
+    });
+
+
+
+});
