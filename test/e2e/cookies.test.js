746a3ae03e30b71eef70d3721bc81097d7b6276c
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-26 17:18:06 -0700

    get all cookies test passes

--- test/e2e/cookies.test.js
+++ test/e2e/cookies.test.js
@@ -8,10 +8,10 @@ describe('Cookies API', () => {
 
   it('initial /GET', () => {
     return request.get('/api/cookies')
-    .then(req => {
-      const cookies = req.body;
-      assert.deepEqual(cookies, []);
-    });
+      .then(req => {
+        const cookies = req.body;
+        assert.deepEqual(cookies, []);
+      });
   });
 
   let courierCookie = {
@@ -34,22 +34,50 @@ describe('Cookies API', () => {
 
   function saveCookie(cookie) {
     return request.post('/api/cookies')
-    .send(cookie)
-    .then(res => res.body);
+      .send(cookie)
+      .then(res => res.body);
   }
 
   it('saves a cookie', () => {
     return saveCookie(courierCookie)
-    .then(saved => {
-      assert.ok(saved._id, 'saved has id');
-      courierCookie = saved;
+      .then(saved => {
+        assert.ok(saved._id, 'saved has id');
+        courierCookie = saved;
+      })
+      .then(() => {
+        return request.get(`/api/cookies/${courierCookie._id}`);
+      })
+      .then(res => res.body)
+      .then(cookie => {
+        assert.deepEqual(cookie, courierCookie);
+      });
+  });
+
+  it('returns 404 if cookie doesnt exist', () => {
+    const notId = '589d04a8b6695bbdfd3106f1';
+    return request.get(`/api/cookies/${notId}`)
+      .then(() => { throw new Error('expected 404'); },
+      res => {
+        assert.equal(res.status, '404');
+      });
+  });
+
+  it('returns list of all cookies', () => {
+    return Promise.all([
+      saveCookie(starbucksCookie),
+      saveCookie(codeFellowCookie)
+    ])
+    .then(savedCookies => {
+      starbucksCookie = savedCookies[0];
+      codeFellowCookie = savedCookies[1];
     })
-    .then(() => {
-      return request.get(`/api/cookies/${courierCookie._id}`);
-    })
+    .then(() => request.get('/api/cookies'))
     .then(res => res.body)
-    .then(cookie => {
-      assert.deepEqual(cookie, courierCookie);
+    .then(cookies => {
+      assert.equal(cookies.length, 3);
+      assert.include(cookies, courierCookie);
+      assert.include(cookies, starbucksCookie);
+      assert.include(cookies, codeFellowCookie);
     });
   });
 
