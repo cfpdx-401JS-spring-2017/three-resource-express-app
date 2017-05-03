f8e65ba60fe359dc060ba89ec16356f6ea5dd37e
Morgan Fogarty <mofo37@gmail.com>
2017-05-02 22:28:54 -0700

    banana-stand e2e initial GET test not passing

--- test/e2e/banana-stands.test.js
+++ test/e2e/banana-stands.test.js
@@ -1,17 +1,17 @@
 const db = require('./_db');
-// const request = require('./_request');
-// const assert = require('chai').assert;
+const request = require('./_request');
+const assert = require('chai').assert;
 
-describe('stores api', () => {
-    
+describe('banana-stand api', () => {
+
   before(db.drop);
 
-    // it('initial /GET returns empty list', () => {
-    //     return request.get('/api/stores')
-    //         .then(req => {
-    //             const bananas = req.body;
-    //             assert.deepEqual(bananas, []);
-    //         });
-    // });
+  it.only('initial /GET returns empty list', () => {
+    return request.get('/api/banana-stands')
+      .then(req => {
+        const bananaStand = req.body;
+        assert.deepEqual(bananaStand, []);
+      });
+  });
 
 });
