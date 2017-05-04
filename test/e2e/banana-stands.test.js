785668f5b24e88a95e439690cfb8a7af31daa978
Morgan Fogarty <mofo37@gmail.com>
2017-05-03 23:12:17 -0700

    'roundtrips a banana stand' test failing

--- test/e2e/banana-stands.test.js
+++ test/e2e/banana-stands.test.js
@@ -6,12 +6,43 @@ describe('banana-stand api', () => {
 
   before(db.drop);
 
-  it.only('initial /GET returns empty list', () => {
+  it('initial /GET returns empty list', () => {
     return request.get('/api/banana-stands')
       .then(req => {
         const bananaStand = req.body;
         assert.deepEqual(bananaStand, []);
       });
+  });
+
+  // let boardwalk = {
+  //   name: 'Boardwalk'
+  // };
+
+  let downtown = {
+    name: 'Downtown'
+  };
+
+  function saveBananaStand(bananaStand) {
+    return request
+      .post('/api/banana-stands')
+      .send(bananaStand)
+      .then(res => res.body);
+  }
+
+  it.only('roundtrips a banana stand', () => {
+    return saveBananaStand(downtown)
+      .then(saved => {
+        assert.ok(saved._id, 'saved has id');
+        downtown = saved;
+      })
+      .then(() => {
+        return request.get(`/api/banana-stands/${downtown._id}`);
+      })
+      .then(res => res.body)
+      .then(got => {
+        assert.deepEqual(got, downtown);
+      });
+
   });
 
 });
