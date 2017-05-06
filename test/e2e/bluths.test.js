464d14c79aff2f8f803fcbfe54bdcb94c7c29938
Morgan Fogarty <mofo37@gmail.com>
2017-05-06 16:57:35 -0700

    initial GET test failing.

---
+++ test/e2e/bluths.test.js
@@ -0,0 +1,17 @@
+const db = require('./_db');
+const request = require('./_request');
+const assert = require('chai').assert;
+
+describe('bluth api', () => {
+
+  before(db.drop);
+
+  it.only('initial GET/ returns empty list', () => {
+    return request.get('/api/bluths')
+      .then(req => {
+        const bluths = req.body;
+        assert.deepEqual(bluths, []);
+      });
+  });
+
+});
